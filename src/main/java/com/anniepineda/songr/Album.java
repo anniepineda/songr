ddd01e45bbe3a8c4d95e9bbd8a964fec16670138
anniepineda <anniepineda@outlook.com>
2020-01-22 00:04:42 -0800

    albums hard coded and rendering on page

--- src/main/java/com/anniepineda/songr/Album.java
+++ src/main/java/com/anniepineda/songr/Album.java
@@ -1,14 +1,14 @@
 package com.anniepineda.songr;
 
 public class Album {
-    String title;
-    String artist;
-    int songCount;
-    int length;
-    String imageUrl;
+    public String  title;
+    public String artist;
+    public int songCount;
+    public int length;
+    public String imageUrl;
 
 
-    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+    public Album(String title, String artist, int songCount, int length, String imageUrl){
         this.title = title;
         this.artist = artist;
         this.songCount = songCount;
@@ -16,6 +16,9 @@ public class Album {
         this.imageUrl = imageUrl;
     }
 
+    public String toString(){
+        return this.title + " " + this.artist + " " + this.songCount + " " + this.length + " ";
+    }
 
 
 }
